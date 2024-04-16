9c101571ecd8b7c513f78b0d0717e359ca94d22f
Gonçalo Heleno <33546359+lentidas@users.noreply.github.com>
2024-04-16 17:00:06 +0200

    feat: add variable to set resources with default values (#27)
    * feat: add variable to set resources with default values
    
    Having default values is good practice to prevent that our components could eventually starve other workloads on the cluster. However, these should probably be adapted in production clusters and are only a safeguard in case someone forgets to set them.
    
    * docs(terraform-docs): generate docs and write to README.adoc
    
    ---------
    
    Co-authored-by: lentidas <lentidas@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -7,6 +7,16 @@ locals {
             "eks.amazonaws.com/role-arn" = var.iam_role_arn != null ? var.iam_role_arn : module.iam_assumable_role_ebs.iam_role_arn
           }
         }
+        resources = {
+          requests = { for k, v in var.resources.controller.requests : k => v if v != null }
+          limits   = { for k, v in var.resources.controller.limits : k => v if v != null }
+        }
+      }
+      node = {
+        resources = {
+          requests = { for k, v in var.resources.node.requests : k => v if v != null }
+          limits   = { for k, v in var.resources.node.limits : k => v if v != null }
+        }
       }
     }
   }]
